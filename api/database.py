e10a7002fb2674e64fc7626f66a91cf486b1b044
snash241 <snash.habib@gmail.com>
2025-09-16 18:18:05 +0200

    definition model

--- api/database.py
+++ api/database.py
@@ -19,15 +19,15 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 #Definition de la Base , qui servira de classe de base pour nos modele SQLAlchemy
 Base = declarative_base()
 
-# tester la connection a la base de donnee
-def test_db_connection():
-    try:
-        #essayer de se connecter a la base de donnee
+#teste code de test
+"""def test_db_connection():
+  try:
+       #essayer de se connecter a la base de donnee
         with engine.connect() as connection:
             print("Connection to the database was successful.")
     except Exception as e:
         print(f"An error occurred while connecting to the database: {e}")
 #appeler la fonction pour tester la connection
-test_db_connection()
+test_db_connection()"""
 
 
